825a7e1a5195db80b82a1ce35a27f38da4811377
Adam Woods-McCormick <awMcCormick@sbgtv.com>
2017-12-20 12:19:02 -0800

    Three basic commands in place, first step toward sublime plugin

---
+++ index.py
@@ -0,0 +1,81 @@
+import sublime;
+from sublime_plugin import EventListener, TextCommand, WindowCommand
+
+from datetime import datetime
+import re
+import pyrebase
+
+email = 'alanbly@gmail.com';
+password = 'u7NV*(Djpcn%7aCgtcAqMdxGS)Y^SX'
+
+config = {
+  'apiKey': 'AIzaSyAgJEKrEdc6Gv6Cwp3ANODIzymloPWJM_s',
+  'authDomain': 'intense-fire-8265.firebaseapp.com',
+  'databaseURL': 'https://intense-fire-8265.firebaseio.com',
+  'projectId': 'intense-fire-8265',
+  'storageBucket': 'intense-fire-8265.appspot.com',
+  'messagingSenderId': '301339768416',
+}
+
+firebase = pyrebase.initialize_app(config)
+
+auth = firebase.auth()
+
+def resource(path, base = None) :
+  base = base if base else firebase.database();
+  segment, subPath = re.sub(r'/+', '/', re.sub(r'^/', '', path)).partition('/')[::2]
+  #print('"' + base.path + '"', '"' + segment + '"', '"' + subPath + '"')
+  return base if not segment else resource(subPath, base.child(segment))
+
+user = auth.sign_in_with_email_and_password(email, password)
+
+def getUserData() :
+  return resource('users/' + user['localId'] + '/metadata');
+
+def getCurrentReason() :
+  userData = getUserData()
+  if not userData :
+    return None;
+  metadata = userData.get().val()
+  return metadata['currentReason'] if metadata and 'currentReason' in metadata else None;
+
+def pushMessage(type, reason = False, date = datetime.now()) :
+  if type :
+    message = {
+      'reason': reason if bool(reason) else 'unknown',
+      'type': type,
+      'date': date.isoformat(),
+    }
+    userMessages = resource('users/' + user['localId'] + '/pain');
+    allMessages = resource('pain');
+    for i in [userMessages, allMessages] :
+      i.push(message);
+
+def setReason(reason) :
+  current = getCurrentReason()
+  if current :
+    pushMessage('DUN', current)
+  getUserData().child('currentReason').set(reason)
+  pushMessage('MUX', reason)
+
+def wtf(reason = getCurrentReason()) :
+  pushMessage('WTF', reason)
+
+def yay(reason = getCurrentReason()) :
+  pushMessage('YAY', reason)
+
+class Prompter(EventListener):
+  def onLoad(self, view):
+    pass
+
+class WtfCommand(TextCommand):
+  def run(self, view, args):
+    pass
+
+class YayCommand(TextCommand):
+  def run(self, view, args):
+    pass
+
+class MuxCommand(TextCommand):
+  def run(self, view, args):
+    pass
